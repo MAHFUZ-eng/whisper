ea522ace93f726a17bc1e897812ca07b437520af
Your Name <your@email.com>
2026-05-10 14:40:01 +0600

    fix: Update hardcoded API URL to ksqnn

--- mobile/lib/axios.ts
+++ mobile/lib/axios.ts
@@ -3,7 +3,7 @@ import * as Sentry from "@sentry/react-native";
 import { useAuth } from "@clerk/clerk-expo";
 import { useCallback } from "react";
 
-const API_URL = "https://whisper-ijeje.sevalla.app/api";
+const API_URL = "https://whisper-ksqnn.sevalla.app/api";
 
 // this is the same thing we did with useEffect setup but it's optimized version - it's better!!
 
